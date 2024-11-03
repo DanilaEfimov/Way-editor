d33f58f6d5e99665a512c44bd03f45420b51e363
DanilaEfimov <efimovdanila.de@gmail.com>
2024-11-04 01:17:00 +0400

    Update Graph.h

--- Release/Graphs/Graph.h
+++ Release/Graphs/Graph.h
@@ -13,18 +13,18 @@ class Graph
 protected:
 	uint V;
 public:
-	Graph(uint _V = 0);											                     // matrix of connectivity & default constructor
+	Graph(uint _V = 0);											// default constructor
 	virtual ~Graph() = 0;
 
 	//===========	ACCESSORS	===========
 	virtual void print(std::fstream& _to) const = 0;
 
 	// ===========	 MATH	===========
-	virtual Graph& operator+(Graph& _Right) = 0;				          // union of graphs
-	virtual Graph& operator+(std::stack<uint>& _Right) = 0;		    // adding new vertex
-	virtual Graph& operator-(Graph& _Right) = 0;				          // difference of graphs
-	virtual Graph& operator-(uint _Vertex) = 0;					          // deleting vertex
-	virtual int operator()(uint _Vertex) = 0;					            // degree of _Vertex (-1 for undefined _V)
+	virtual Graph& operator+(Graph& _Right) = 0;				// union of graphs
+	virtual Graph& operator+(std::stack<uint>& _Right) = 0;		// adding new vertex
+	virtual Graph& operator-(Graph& _Right) = 0;				// difference of graphs
+	virtual Graph& operator-(uint _Vertex) = 0;					// deleting vertex
+	virtual int operator()(uint _Vertex) = 0;					// degree of _Vertex (-1 for undefined _V)
 };
 
 #endif
@@ -36,10 +36,10 @@ public:
 **	Graph --+
 **			|
 **			+ UDirGraph ----+
-**			|				        |
-**			|				        + WGraph	(weighted graph)
+**			|				|
+**			|				+ WGraph	(weighted graph)
 **			|
 **			+ DirGraph -----+
-**							        |
-**						        	+ WDGraph
+**							|
+**							+ WDGraph
 ****************************************************************/
