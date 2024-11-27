c15276f8e35f7a9c2d70093ec1a90ad28aa75e97
DanilaEfimov <efimovdanila.de@gmail.com>
2024-11-27 23:56:13 +0400

    Add files via upload

--- Release/main.cpp
+++ Release/main.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include"Graphs/UDirGraph.h"
-#include"Graphs/DirGraph.h"
 #include<ctime>
 
 int main(int argc, char *argv[])
@@ -9,18 +8,24 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     std::srand(time(0));
-    int size = rand()%50 + 20;
+    int size = 5;
     byte** mat = new byte*[size];
     for(int i = 0; i < size; i++){
         mat[i] = new byte[size];
         for(int j = 0; j < size; j++){
-            mat[i][j] = rand()%2;
+            mat[i][j] = (i+j)%3%2;
         }
     }
-    DirGraph G1(size, mat);
     UDirGraph G2(size, mat);
     std::fstream output("E:/Qt creator/Way_Editor/test.txt");
-    G1.print(output);
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            output << (mat[i][j] ? 1 : 0) << ' ';
+        }
+        output << '\n';
+    }
+    G2.print(output);
+    G2 = G2 - 2;
     G2.print(output);
     output.close();
     for(size_t i = 0; i < size; i++){
